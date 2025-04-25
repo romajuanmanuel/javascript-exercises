cc8d5e50f9d8712bdbba046e7862a1e4d3bf428d
romaxolote <romajuanmanuel@gmail.com>
2025-04-25 12:43:01 -0300

    ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArray = str.split('');
+    const newArray = [];
+    for (let i = strArray.length - 1; i >= 0; i--) {
+        newArray.push(strArray[i]);
+    }
+    return newArray.join('');
 };
 
 // Do not edit below this line
